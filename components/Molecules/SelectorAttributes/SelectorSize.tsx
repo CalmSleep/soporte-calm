f45696dd067fa17365ef1f48b3cf5227e6ca2fcf
SofiaKauffer <126362555+SKaufferSofia@users.noreply.github.com>
2025-05-06 14:58:41 -0300

    error build

--- components/Molecules/SelectorAttributes/SelectorSize.tsx
+++ components/Molecules/SelectorAttributes/SelectorSize.tsx
@@ -1,12 +1,19 @@
-import variations_sizes from "@/utils/variations_sizes"
-import { ContainerSize, HeightContainer, TextSelected } from "./styled"
-import { IPropsSize } from "./types"
-import SizeCard from "../SizeCard/SizeCard"
-import SkeletonLoader from "../SkeletonLoader/SkeletonLoader"
-import { Arrow, DropdownContainer, DropdownHeader, DropdownList, DropdownListContainer, ListItem } from "@/components/Organisms/MainBlock/styled"
-import { ArrowQuantity } from "@/components/Organisms/MainBlock/mainBlockicons"
-import { useEffect, useRef, useState } from "react"
-import Text from "@/components/Atoms/Typography/Text"
+import variations_sizes from "@/utils/variations_sizes";
+import { ContainerSize, HeightContainer, TextSelected } from "./styled";
+import { IPropsSize } from "./types";
+//import SizeCard from "../SizeCard/SizeCard"
+import SkeletonLoader from "../SkeletonLoader/SkeletonLoader";
+import {
+  Arrow,
+  DropdownContainer,
+  DropdownHeader,
+  DropdownList,
+  DropdownListContainer,
+  ListItem,
+} from "@/components/Organisms/MainBlock/styled";
+import { ArrowQuantity } from "@/components/Organisms/MainBlock/mainBlockicons";
+import { useEffect, useRef, useState } from "react";
+import Text from "@/components/Atoms/Typography/Text";
 
 const SelectorSize = ({
   arrChildren,
@@ -19,21 +26,24 @@ const SelectorSize = ({
   isCategory,
 }: IPropsSize) => {
   const [isSizeOpen, setIsSizeOpen] = useState(false);
-  const dropdownRef = useRef<HTMLDivElement>(null);  
+  const dropdownRef = useRef<HTMLDivElement>(null);
 
   const regex = /\((.*?)\)/g;
-  
+
   useEffect(() => {
     const handleClickOutside = (e: MouseEvent) => {
-      if (dropdownRef.current && !dropdownRef.current.contains(e.target as Node)) {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(e.target as Node)
+      ) {
         setIsSizeOpen(false);
       }
     };
 
-    window.addEventListener('click', handleClickOutside);
+    window.addEventListener("click", handleClickOutside);
 
     return () => {
-      window.removeEventListener('click', handleClickOutside);
+      window.removeEventListener("click", handleClickOutside);
     };
   }, []);
 
@@ -43,16 +53,18 @@ const SelectorSize = ({
     } else if (text) {
       return text;
     }
-  }
+  };
 
-  const selectedSize =  textFormater(variations_sizes[
-    selected?.attributes[
-      valToSearch as keyof typeof selected.attributes
-    ] as keyof typeof variations_sizes
-  ])
+  const selectedSize = textFormater(
+    variations_sizes[
+      selected?.attributes[
+        valToSearch as keyof typeof selected.attributes
+      ] as keyof typeof variations_sizes
+    ]
+  );
 
   return (
-  /*   <ContainerSize>
+    /*   <ContainerSize>
       {arrChildren && arrChildren.length > 0 ? (
         arrChildren.map((child) => (
           <SizeCard
@@ -78,111 +90,109 @@ const SelectorSize = ({
       )}
     </ContainerSize> */
 
-
     <HeightContainer>
-      <DropdownContainer $isSize onClick={() => setIsSizeOpen(prevState => !prevState)}  ref={dropdownRef}>
+      <DropdownContainer
+        $isSize
+        onClick={() => setIsSizeOpen((prevState) => !prevState)}
+        ref={dropdownRef}
+      >
         <DropdownHeader>
-           {
-              selectedSize && (
-              <TextSelected>
+          {selectedSize && (
+            <TextSelected>
+              <Text
+                font={isCategory ? "bold" : "medium"}
+                align="center"
+                responsiveMobile={{
+                  width: "auto",
+                  fontSize: "0.85rem",
+                }}
+              >
+                {Array.isArray(selectedSize) ? selectedSize[0] : selectedSize}
+              </Text>
+
+              {Array.isArray(selectedSize) && (
                 <Text
-                  font={isCategory ? "bold" : "medium"}
+                  font="medium"
                   align="center"
+                  color="millionGray"
                   responsiveMobile={{
-                    width:"auto",
-                    fontSize:"0.85rem"
+                    width: "auto",
+                    fontSize: "0.75rem",
                   }}
                 >
-                    {Array.isArray(selectedSize) ? selectedSize[0] : selectedSize}
+                  ({selectedSize[1]})
                 </Text>
-
-                {
-                  Array.isArray(selectedSize) &&
-                  <Text
-                    font="medium"
-                    align="center"
-                    color="millionGray"
-                    responsiveMobile={{
-                      width:"auto",
-                      fontSize:"0.75rem"
-                      }}
-                  >
-                      ({selectedSize[1]})
-                  </Text>
-                }
-              </TextSelected> 
-              )
-            }
+              )}
+            </TextSelected>
+          )}
         </DropdownHeader>
 
-        <Arrow $isOpen={isSizeOpen}>
-          {ArrowQuantity()}
-        </Arrow>
+        <Arrow $isOpen={isSizeOpen}>{ArrowQuantity()}</Arrow>
 
         {isSizeOpen && (
           <DropdownListContainer>
             <DropdownList>
-            {(arrChildren && arrChildren.length > 0) && (
-              arrChildren.map((option, index) => {
-                const size =  textFormater(variations_sizes[
-                  option.attributes[
-                    valToSearch as keyof typeof option.attributes
-                  ] as keyof typeof variations_sizes
-                ])
+              {arrChildren &&
+                arrChildren.length > 0 &&
+                arrChildren.map((option, index) => {
+                  const size = textFormater(
+                    variations_sizes[
+                      option.attributes[
+                        valToSearch as keyof typeof option.attributes
+                      ] as keyof typeof variations_sizes
+                    ]
+                  );
 
-                return (
-                <ListItem 
-                key={index} 
-                $isSelected={selected ? selected.id === option.id : false}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setSelected(option.id)
-                  setIsSizeOpen(false);
-                  hasRenders && setIsSizeChange(true)
-                }}
-                $isLast={index === arrChildren.length - 1}
-                $isFirst={index === 0}
-                >
-                   {
-                    size && (
-                    <TextSelected>
-                      <Text
-                        font={isCategory ? "bold" : "medium"}
-                        align="left"
-                        responsiveMobile={{
-                          width:"auto",
-                          fontSize:"0.85rem"
-                        }}
-                      >
-                          {Array.isArray(size) ? size[0] : size}
-                      </Text>
-
-                      {
-                        Array.isArray(size) &&
-                        <Text
-                          font="medium"
-                          align="left"
-                          color="millionGray"
-                          responsiveMobile={{
-                            width:"auto",
-                            fontSize:"0.75rem"
+                  return (
+                    <ListItem
+                      key={index}
+                      $isSelected={selected ? selected.id === option.id : false}
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setSelected(option.id);
+                        setIsSizeOpen(false);
+                        hasRenders && setIsSizeChange(true);
+                      }}
+                      $isLast={index === arrChildren.length - 1}
+                      $isFirst={index === 0}
+                    >
+                      {size && (
+                        <TextSelected>
+                          <Text
+                            font={isCategory ? "bold" : "medium"}
+                            align="left"
+                            responsiveMobile={{
+                              width: "auto",
+                              fontSize: "0.85rem",
                             }}
-                        >
-                            ({size[1]})
-                        </Text>
-                      }
-                    </TextSelected> )
-                  }
-                </ListItem>
-              )})
-            )
-            }
+                          >
+                            {Array.isArray(size) ? size[0] : size}
+                          </Text>
+
+                          {Array.isArray(size) && (
+                            <Text
+                              font="medium"
+                              align="left"
+                              color="millionGray"
+                              responsiveMobile={{
+                                width: "auto",
+                                fontSize: "0.75rem",
+                              }}
+                            >
+                              ({size[1]})
+                            </Text>
+                          )}
+                        </TextSelected>
+                      )}
+                    </ListItem>
+                  );
+                })}
             </DropdownList>
           </DropdownListContainer>
         )}
       </DropdownContainer>
     </HeightContainer>
-  )
-}
+  );
+};
 
-export default SelectorSize
+export default SelectorSize;
